a470c339be2b1175e3baccfacab4671e7de79383
Utkarsh13tiwari <93029055+Utkarsh13tiwari@users.noreply.github.com>
2022-08-10 05:35:43 +0530

    Update code.py

--- code.py
+++ code.py
@@ -6,42 +6,43 @@ from boltiot import Bolt,Sms
 
 bolt=Bolt(conf.BOLT_API,conf.DEVICE_ID)#we have configured our module connections
 sms=Sms(conf.SSID,conf.AUTH_TOKEN,conf.TO_NUMBER,conf.FROM_NUMBER)# our twillio account is connected to our code
-mail = Email(conf.MAIL_API, conf.SANDBOX_URL, conf.SENDER_MAIL, conf.RECEIVER_MAIL) #our mailgun account is also connected to our code now
 
+print("Whcih crypto are you interested in ")
+print(" 1) BTC \n 2) ETH \n 3) BUSD \n 4) FIL \n 5) USDT ")
 
-print("Select any one of the following currency input\nINR\nUSD\nJPY\nEUR")
-currency=input("Enter the above Currency from which you have to invest in:")#we chose the currency which we want to invest in
-sell_price=float(input("Enter Your Selling Price:"))#now we input our desired selling price.
+coin=str(input("Enter your choice: " )) #we take input of from the user
+
+sell_price=float(input("Enter the  Sell price: ")) #input of sellin price
+buy_price=float(input("Enter the Buy price: ")) #input of the buying price
+
 
 # following is the function definition which will be called later
 def price_check():
-    url=("https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms={}".format(currency.upper()))#currency.upper converts string into uppercase letters.
+    url=("https://min-api.cryptocompare.com/data/price?fsym={}}&tsyms='USD'".format(coin.upper())) #coin.upper converts string into uppercase letters.
     response=requests.request("GET",url)
     response=json.loads(response.text)
-    current_price=response[currency.upper()]
+    current_price=response['USD']
     return current_price
 
 
 while True:
-    market_price=price_check()
-    print("Market price of Bitcoin is:",market_price)#this prints current market price of Bitcoin in the specified country currency.
-    print("Selling Price is:",sell_price)#this gives the Selling price we have entered.
-    try:
-         if market_price < sell_price:#this checks for current market price bitcoin is less than of selling price or not.      
-             bolt.digitalWrite("0","HIGH")#LED gets "ON
-             response1=sms.send_sms("The Bitcoins are at price ={} You can Invest now if you want".format(current_bitcoin_value))
-             print("Status of Sms at Twilo:"+str(response1.status))#Prints status of twilio account whether message reached or not.
-             response = mail.send_email("Alert","The Bitcoins are at price ={} You can Invest now if you want".format(current_bitcoin_value))
-            
-          elif market_price < sell_price:#this checks for current market price bitcoin is greater than of selling price or not.
-              bolt.digitalWrite("1","HIGH")#BUZZER gets "ON
-             response1=sms.send_sms("The Bitcoins are at price ={} You need to be cautious".format(current_bitcoin_value))
-             print("Status of Sms at Twilo:"+str(response1.status))#Prints status of twilio account whether message reached or not.
-             response = mail.send_email("CAUTION","The Bitcoins are at price ={} You need to be cautious".format(current_bitcoin_value))
-    except Exception as e:
-        print("An error occured\n")
-        print(e)
-    time.sleep(5).
-    my_bolt.digitalWrite("0","LOW")#led gets off
-    my_bolt.digitalWrite("1","LOW")#buzzer gets off
-    time.sleep(30)
+      market_price=price_check()
+      print("Market price: ",market_price)
+      try:
+           if market_price<buy_price:
+              mybolt.digitalWrite("0","HIGH")  #if market price is more then our selling price then buzzer starts beeping
+              response1=sms.send_sms("\nMarket value has just hit your Buying price ") #Twilio sends SMS to our phone
+              print("Status of SMS "+str(response1.status)) 
+
+           elif market_price>sell_price:
+              print("\nMarket Price is more then your selling price\n ")
+              mybolt.digitalWrite('0','HIGH')   #if market price is more then our selling price then buzzer starts beeping 
+              response1=sms.send_sms("Market price has just crossed your selling price ")
+              print("Status of SMS "+str(response1.status)) # twilio send message your number 
+
+
+      except Exception as e:
+           print(e)
+      time.sleep(2) #introducing time delay 
+      mybolt.digitalWrite("0","LOW") #off the buzzer 
+      time.sleep(10)
